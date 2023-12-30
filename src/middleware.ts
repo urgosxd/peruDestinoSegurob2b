e49ee92c89d83fb6c0ef6885e1f641dd390a94d6
chamba <urgosxd@gmail.com>
2023-12-30 09:29:26 -0800

    aoe

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-// export {default} from "next-auth/middleware"
+export {default} from "next-auth/middleware"
 
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  const session = !!req.cookies.get("next-auth.session-token")
-  console.log(session)
+// export default async function middleware(req: NextRequest) {
+//   const path = req.nextUrl.pathname;
+//   const session = !!req.cookies.get("next-auth.session-token")
+//   console.log(session)
 
-  if (!session) {
-    return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
-  }
-  return NextResponse.next();
-}
-
-// export const config = {
-//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)','/boards', '/workspace/:path*']
+//   if (!session) {
+//     return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
+//   }
+//   return NextResponse.next();
 // }
+
+// // export const config = {
+// //   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)','/boards', '/workspace/:path*']
+// // }
 
 export const config = {
   matcher : ["/dashboard"]
