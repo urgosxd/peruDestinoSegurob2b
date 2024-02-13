b47ad394704cd523de521a5b92c65ce00910e858
chamba <urgosxd@gmail.com>
2024-02-13 06:31:30 -0800

    aaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head />
-      
+      <head/>
       <Theme>
       <body className={font.className+ ""}>
+         <Script dangerouslySetInnerHTML={{__html: `(function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.es/b15344011/crm/site_button/loader_7_qznmsh.js');`}}/>
         {children}
       </body>
       </Theme>
