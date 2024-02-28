c6e1cee40c91a2743ee06a508892e78a5068e00c
chamba <urgosxd@gmail.com>
2024-02-28 10:04:24 -0800

    aoe

---
+++ src/app/(pages)/formulario/page.tsx
@@ -0,0 +1,42 @@
+// 'use client'
+// import { useState } from "react";
+
+
+export default async  function Formulario() {
+  // const data:any[] = await getContactoPage()
+  // const contactoUnico = data[0]
+  // console.log(data);
+  
+  // const [ip,setIp] = useState("")
+
+  //  const getData = async () => {
+  //   const res = await fetch("https://api.ipify.org/?format=json");
+  //   if(res.ok){
+  //     const data = await res.json()
+  //     return data
+  //   }
+  // }
+  // const ip = await getData()
+
+  return(
+  <div className="flex flex-col w-[98vw] items-center">
+      {/* <BackBanner imgSrc={contactoUnico.acf.imgback} txt="Contacto" /> */}
+      {/* <div className="flex lg:flex-row flex-col w-full mt-12"> */}
+
+      {/* <div className="lg:w-2/3 w-full  flex justify-center"> */}
+
+      {/* <ContactoForm action={addClient}/> */}
+      {/* </div> */}
+      {/* <div className="lg:w-1/3 w-full flex lg:justify-center"> */}
+      {/* <FakeFooter phones={contactoUnico.acf.telefono.split(",")} address={contactoUnico.acf.direccion} email={contactoUnico.acf.correo}/> */}
+      {/* </div> */}
+      {/* </div> */}
+        FORMULARIOOO
+    </div>
+  )
+  }
+
+export const metadata = {
+  title: "Contacto Peru Destino Seguro",
+  description: "**",
+}
