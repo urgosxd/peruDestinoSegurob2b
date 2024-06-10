03bc8e36c5ec8837014fa61beca1b1a72c7dea90
chamba <urgosxd@gmail.com>
2024-06-10 15:32:41 -0700

    aoe

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -7,7 +7,7 @@ import ProfileCard from '@/components/cardPaquetes'
 // import paquetes from "@/app/lib/paquetes"
 // import destinos from './lib/destinos'
 import BackCard from '@/components/backCard'
-import { getAllDestinos, getAllPaquetes, getAllImgCarousel, getAllPregFrecuentes } from '@/app/lib/wp'
+import {getDestinos, getInicioPage} from '@/app/lib/wp'
 import { GetStaticProps, NextPage } from 'next'
 import Questions from '@/components/questions'
 import AgenciaForm from '@/components/formAgencio'
@@ -16,42 +16,48 @@ interface Props {
 }
 
 export default async function Home({ }: Props) {
-  let paquetes: any[] = await getAllPaquetes()
-  paquetes = paquetes.reverse()
-  // paquetes = paquetes.map(ele => ele.acf).reverse()
+  // let paquetes: any[] = await getAllPaquetes()
+  // paquetes = paquetes.reverse()
+  // // paquetes = paquetes.map(ele => ele.acf).reverse()
 
-  let destinos: any[] = await getAllDestinos()
-  destinos = destinos.map(ele => ele.acf).reverse()
+  // let destinos: any[] = await getAllDestinos()
+  // destinos = destinos.map(ele => ele.acf).reverse()
 
-  let imgCarousels: any[] = await getAllImgCarousel()
-  imgCarousels = imgCarousels.map(ele => ele.acf)
+  // let imgCarousels: any[] = await getAllImgCarousel()
+  // imgCarousels = imgCarousels.map(ele => ele.acf)
 
-  let pregFrecuents: any[] = await getAllPregFrecuentes()
-  pregFrecuents = pregFrecuents.map(ele => ele.acf).reverse()
-  // console.log(paquetes)
+  // let pregFrecuents: any[] = await getAllPregFrecuentes()
+  // pregFrecuents = pregFrecuents.map(ele => ele.acf).reverse()
+  // // console.log(paquetes)
   // console.log(imgCarousels);
   // console.log(pregFrecuents);
 
+  const dataGeneral = await getInicioPage()
+
+  const destinos = await getDestinos(['name','background'])
+  console.log(dataGeneral)
+  console.log(destinos)
+  
   return (
     <div className="flex flex-col items-center">
-      <CustomCarousel imgs={imgCarousels}></CustomCarousel>
+      {/* <CustomCarousel imgs={imgCarousels}></CustomCarousel> */}
       {/* <SessionProvider> */}
       {/* <SessionValidator>GAAAAAAAA</SessionValidator> */}
       {/* </SessionProvider> */}
       <Subtitle>Nuestros Paquetes</Subtitle>
       <div className="grid lg:grid-cols-3 lg:gap-3 justify-items-center w-10/12 grid-cols-1 gap-2 pl-7 lg:pl-0 gap-y-10">
-        {paquetes.map(ele => (<ProfileCard key={ele.acf.txtunico} imgSrc={ele.acf.imgsrc} txt1={ele.acf.txtunico} txt2={ele.acf.precio} slug={ele.slug} time={ele.acf.tiempo} incluido={ele.acf.incluido} excluido={ele.acf.excluido} />))}
+        {/* {paquetes.map(ele => (<ProfileCard key={ele.acf.txtunico} imgSrc={ele.acf.imgsrc} txt1={ele.acf.txtunico} txt2={ele.acf.precio} slug={ele.slug} time={ele.acf.tiempo} incluido={ele.acf.incluido} excluido={ele.acf.excluido} />))} */}
       </div>
       <Subtitle >Destinos</Subtitle>
       <div className="grid lg:grid-cols-3 lg:gap-3 justify-items-center destinoCss w-10/12 pl-0 grid-cols-1 gap-y-2 lg:pl-0">
-        {destinos.map(ele => (<BackCard key={ele.txt_unique} imgSrc={ele.imgsrc} txt={ele.txt_unique} />))}
+        {/* {destinos.map(ele => (<BackCard key={ele.txt_unique} imgSrc={ele.imgsrc} txt={ele.txt_unique} />))} */}
       </div>
 
       <div className="flex lg:flex-row flex-col w-full mt-10">
 
         <div className="lg:w-1/2 w-full flex flex-col items-center">
           <h3 className="my-9 font-normal text-3xl">Preguntas Frecuentes</h3>
-          <Questions questionAnswer={pregFrecuents} />
+          {/* <Questions questionAnswer={pregFrecuents} /> */}
 
         </div>
         <div className="lg:w-1/2 w-full p-10 lg:p-0">
--- src/app/lib/wp.ts
+++ src/app/lib/wp.ts
@@ -1,8 +1,9 @@
 import axios from "axios"
-const API_URL = process.env.WORDPRESS_API_URL!!
+// const API_URL = process.env.WORDPRESS_API_URL!!
+const API_URL = "https://b2bcms.onrender.com/api/v2/"
 
 async function fetchAPI(query = '', variable:string[] =[ ]) {
-  console.log(API_URL + "GAAAAAAAAAAAAAAa")
+  // console.log(API_URL + "GAAAAAAAAAAAAAAa")
   const headers: any = { 'Content-Type': 'application/json' }
 
   // if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
@@ -13,7 +14,7 @@ async function fetchAPI(query = '', variable:string[] =[ ]) {
 
   if (variable.length > 0) {
     try {
-      const res = await axios.get(API_URL + query + '?acf_format=standard&' + variable.join("&"))
+      const res = await axios.get(API_URL + query + '?fields=' + variable.join(","))
       const data = await res.data
       return data
     }
@@ -29,7 +30,7 @@ async function fetchAPI(query = '', variable:string[] =[ ]) {
   } else {
 
     try {
-      const res = await axios.get(API_URL + query + '?acf_format=standard')
+      const res = await axios.get(API_URL + query )
       const data = await res.data
       return data
     }
@@ -62,74 +63,83 @@ async function fetchAPI(query = '', variable:string[] =[ ]) {
 //   return data.post
 // }
 
-export async function getAllPaquetes() {
-  const data = await fetchAPI('paquete/')
-  return data
-}
-export async function getAllDestinos() {
-  const data = await fetchAPI('destino/')
-  return data
-}
-export async function getAllImgCarousel() {
-  const data = await fetchAPI('imgcarousel/')
-  return data
-}
-export async function getAllPregFrecuentes() {
-  const data = await fetchAPI('pregfrecuente/')
+export async function getInicioPage() {
+  const data = await fetchAPI('pages/inicio/4')
   return data
 }
 
-export async function getNosotrosPage() {
-  const data = await fetchAPI('nosotros_page/')
+export async function getDestinos(variables:string[]){
+  const data = await fetchAPI('snippets/destino/',variables)
   return data
 }
+// export async function getAllPaquetes() {
+//   const data = await fetchAPI('paquete/')
+//   return data
+// }
+// export async function getAllDestinos() {
+//   const data = await fetchAPI('destino/')
+//   return data
+// }
+// export async function getAllImgCarousel() {
+//   const data = await fetchAPI('imgcarousel/')
+//   return data
+// }
+// export async function getAllPregFrecuentes() {
+//   const data = await fetchAPI('pregfrecuente/')
+//   return data
+// }
 
-export async function getPaqueteBySlug(slug: string) {
-  const data = await fetchAPI("paquete/", [`slug=${slug}`])
-  return data
-}
-export async function getContactoPage() {
-  const data = await fetchAPI('contacto_page/')
-  return data
-}
-export async function getBlogPage() {
-  const data = await fetchAPI('blog_page/')
-  return data
-}
-export async function getCalendarioPage() {
-  const data = await fetchAPI('calendario_page/')
-  return data
-}
+// export async function getNosotrosPage() {
+//   const data = await fetchAPI('nosotros_page/')
+//   return data
+// }
 
-export async function get9Posts(page:number) {
-  const data = await fetchAPI('posts/',[`per_page=9`,`page=${page}`])
-  return data
-}
-export async function getPostBySlug(slug: string) {
-  const data = await fetchAPI("posts/", [`slug=${slug}`])
-  return data
-}
-export async function getDestinosPage() {
-  const data = await fetchAPI('destinos_page/')
-  return data
-}
-export async function getDestinoCusco() {
-  const data = await fetchAPI('destinos_cusco/')
-  return data
-}
-export async function getDestinoArequipa() {
-  const data = await fetchAPI('destinos_arequipa/')
-  return data
-}
-export async function getDestinoPuno() {
-  const data = await fetchAPI('destinos_puno/')
-  return data
-}
-export async function getAllSalidasGrupales() {
-  const data = await fetchAPI('salida_grupal/')
-  return data
-  }
-export async function getSalidasBySlug(slug:string) {
-  const data = await fetchAPI('salida_grupal/',[`slug=${slug}`])
-  return data
-  }
+// export async function getPaqueteBySlug(slug: string) {
+//   const data = await fetchAPI("paquete/", [`slug=${slug}`])
+//   return data
+// }
+// export async function getContactoPage() {
+//   const data = await fetchAPI('contacto_page/')
+//   return data
+// }
+// export async function getBlogPage() {
+//   const data = await fetchAPI('blog_page/')
+//   return data
+// }
+// export async function getCalendarioPage() {
+//   const data = await fetchAPI('calendario_page/')
+//   return data
+// }
+
+// export async function get9Posts(page:number) {
+//   const data = await fetchAPI('posts/',[`per_page=9`,`page=${page}`])
+//   return data
+// }
+// export async function getPostBySlug(slug: string) {
+//   const data = await fetchAPI("posts/", [`slug=${slug}`])
+//   return data
+// }
+// export async function getDestinosPage() {
+//   const data = await fetchAPI('destinos_page/')
+//   return data
+// }
+// export async function getDestinoCusco() {
+//   const data = await fetchAPI('destinos_cusco/')
+//   return data
+// }
+// export async function getDestinoArequipa() {
+//   const data = await fetchAPI('destinos_arequipa/')
+//   return data
+// }
+// export async function getDestinoPuno() {
+//   const data = await fetchAPI('destinos_puno/')
+//   return data
+// }
+// export async function getAllSalidasGrupales() {
+//   const data = await fetchAPI('salida_grupal/')
+//   return data
+//   }
+// export async function getSalidasBySlug(slug:string) {
+//   const data = await fetchAPI('salida_grupal/',[`slug=${slug}`])
+//   return data
+//   }
