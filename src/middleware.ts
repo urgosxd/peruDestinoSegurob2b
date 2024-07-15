54378d2f8cb973a5bfc047aabb2c04b3649becbb
chamba <urgosxd@gmail.com>
2024-07-15 12:59:41 -0700

    aoe

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
-export {default} from "next-auth/middleware"
 
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import Negotiator from "negotiator";
+import { match } from "@formatjs/intl-localematcher";
 
 // export default async function middleware(req: NextRequest) {
 //   const path = req.nextUrl.pathname;
@@ -20,8 +21,45 @@ export {default} from "next-auth/middleware"
 
 
 
+const locales = ["es", "en"];
+const defaultLocale = "es";
+const cookieName = "i18nlang";
+
+// Get the preferred locale, similar to the above or using a library
+function getLocale(request: NextRequest): string {
+  // Get locale from cookie
+  if (request.cookies.has(cookieName))
+    return request.cookies.get(cookieName)!.value;
+  // Get accept language from HTTP headers
+  const acceptLang = request.headers.get("Accept-Language");
+  if (!acceptLang) return defaultLocale;
+  // Get match locale
+  const headers = { "accept-language": acceptLang };
+  const languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/_next")) return NextResponse.next();
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+  if (pathnameHasLocale) return;
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  const response = NextResponse.redirect(request.nextUrl);
+  // Set locale to cookie
+  response.cookies.set(cookieName, locale);
+  return response;
+}
+
 export const config = {
   // matcher : ["/oaeu"],
-  matcher : ["/dashboard/:path*"]
+  matcher : ["/((?!_next).*)","/dashboard/:path*"]
 }
 
