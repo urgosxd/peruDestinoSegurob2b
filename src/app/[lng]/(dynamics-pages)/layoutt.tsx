0ce317dee51c9a2ef743bbff13d8b2693ca5c1e7
chamba <urgosxd@gmail.com>
2024-08-01 16:29:18 -0700

    aoe

--- src/app/[lng]/(dynamics-pages)/layoutt.tsx
+++ src/app/[lng]/(dynamics-pages)/layoutt.tsx
@@ -3,6 +3,9 @@ import { Footer } from "@/components/footer"
 import { NavbarDefault } from "@/components/navbar"
 import Script from 'next/script'
 
+
+export const fetchCache = 'force-no-store';
+
 export default async function Layout({children,params}:{children:React.ReactNode,params:any}){
   console.log(params)
   const destinos = await getDestinos({fields:"*"})
