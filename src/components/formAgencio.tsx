c9acdc237d8b1b5450e6c6c02dcc3417fe2e7847
chamba <urgosxd@gmail.com>
2024-08-23 11:42:35 -0700

    aoe

--- src/components/formAgencio.tsx
+++ src/components/formAgencio.tsx
@@ -14,10 +14,10 @@ interface Props {
 export default function AgenciaForm({lng}:Props) {
   return (
 
-    <form className="lg:mt-9 mt-3 mb-2 w-full max-w-screen-lg pr-10">
+    <form className="lg:mt-3 mt-3 mb-2 w-full max-w-screen-lg pr-10">
       <div className="mb-1 flex flex-col lg:gap-6 gap-2 justify-content">
-       <Button className="mt-6 text-white bg-[#D20000] lg:text-xl text-lg !px-3 w-24 lg:w-2/4" >
-          <Link href={`/${lng}/contactar`}>
+       <Button className=" text-white bg-[#D20000] lg:text-xl text-lg !px-0 w-fit lg:w-2/4" >
+          <Link href={`/${lng}/contactar`} className="w-full block px-5">
           Contactar
           </Link>
         </Button>
