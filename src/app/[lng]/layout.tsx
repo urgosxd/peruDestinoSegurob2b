ea5d162e182077752ec735f28281ba67422a292f
chamba <urgosxd@gmail.com>
2024-08-27 13:14:39 -0700

    aa

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -41,16 +41,16 @@ export default async function Layout({ children, params: { lng } }: LocaleRouteL
         className={`${font.variable} ${font2.variable} ${myFont.variable} font-sans`}
       >
         <NavbarDefault lng={lng} destinos={destinosObject} />
-        <main>
-          <Script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,u){
-                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn.bitrix24.es/b15344011/crm/site_button/loader_7_qznmsh.js');`}} />
+        {/* <main> */}
+        {/*   <Script dangerouslySetInnerHTML={{ */}
+        {/*     __html: `(function(w,d,u){ */}
+        {/*         var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0); */}
+        {/*         var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h); */}
+        {/* })(window,document,'https://cdn.bitrix24.es/b15344011/crm/site_button/loader_7_qznmsh.js');`}} /> */}
 
-          {children}
-          <Script dangerouslySetInnerHTML={{ __html: `function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"2ee835c433b36ed62861f1b5ba0aa7c8"})});` }} />
-        </main>
+        {/*   {children} */}
+        {/*   <Script dangerouslySetInnerHTML={{ __html: `function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"2ee835c433b36ed62861f1b5ba0aa7c8"})});` }} /> */}
+        {/* </main> */}
 
 
         <Suspense fallback={<div>Loading footer...</div>}>
