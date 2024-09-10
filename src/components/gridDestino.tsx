593929fea0b882c8dcfef5b0b74ce32a031be80b
chamba <urgosxd@gmail.com>
2024-09-10 12:55:24 -0700

    aoe

--- src/components/gridDestino.tsx
+++ src/components/gridDestino.tsx
@@ -8,31 +8,40 @@ import { LocaleType } from "../../i18next/settings";
 import Link from "next/link";
 import { useMobile } from "@/hooks/useMobile";
 import { SwipeCarousel } from "./swiperCarousel";
+import MotionElement from "@/components/clientExportElement"
 
 type FinalFrame = {
   imgSrc: string
   label: string
-  imgMobileSrc:string
+  imgMobileSrc: string
 }
 
 
-export const RevealBento = ({ img, imgs,urls,lng }: { img: FinalFrame, imgs: FinalFrame[],urls:string[],lng:LocaleType }) => {
-  
+export const RevealBento = ({ img, imgs, urls, lng }: { img: FinalFrame, imgs: FinalFrame[], urls: string[], lng: LocaleType }) => {
+
   const { t } = useTranslation(lng, 'translation')
-  function putNameDestiny (dest:string){
+  function putNameDestiny(dest: string) {
     const baseURL = `/${lng}/${t('destinations')}/?city=${dest}`
     return baseURL
   }
 
   const isMobile = useMobile()
-  
-  const getImages = (obs)=> obs.map(ele=>({src:ele.imgMobileSrc}))
-  const getLabelsImgs = (obs)=> obs.map(ele=>({src:ele.imgMobileSrc,label:ele.label}))
-  
+
+  const getImages = (obs) => obs.map(ele => ({ src: ele.imgMobileSrc }))
+  const getLabelsImgs = (obs) => obs.map(ele => ({ src: ele.imgMobileSrc, label: ele.label }))
+
   const globalItems = [img].concat(imgs)
 
-  
-  return isMobile ?<div className=""> <SwipeCarousel imgs={getImages(globalItems)} labelImgs={getLabelsImgs(globalItems)} label /> </div> : <div className=" w-full bg-zinc-900 px-4 text-zinc-50">
+
+  return isMobile ? <div className=""> <SwipeCarousel imgs={getImages(globalItems)} labelImgs={getLabelsImgs(globalItems)} label /> </div> :
+    <MotionElement
+      as="div"
+      initial={{ opacity: 0, y: 200 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, type: 'spring' }}
+      viewport={{ amount: 0.3 }}
+
+      className=" w-full bg-zinc-900 px-4 text-zinc-50">
       <motion.div
         initial="initial"
         animate="animate"
@@ -42,9 +51,9 @@ export const RevealBento = ({ img, imgs,urls,lng }: { img: FinalFrame, imgs: Fin
         className="mx-auto grid max-w-[90%] grid-flow-dense grid-cols-12 gap-4"
       >
         <HeaderBlock img={img} url={putNameDestiny(urls[0])} />
-        <SocialsBlock imgs={imgs} urls={urls.slice(1,urls.length).map(ele=> putNameDestiny(ele))} />
-        </motion.div>
-          </div>
+        <SocialsBlock imgs={imgs} urls={urls.slice(1, urls.length).map(ele => putNameDestiny(ele))} />
+      </motion.div>
+    </MotionElement>
 
 };
 
@@ -83,7 +92,7 @@ const Block = ({ className, ...rest }: BlockProps) => {
   );
 };
 
-const HeaderBlock = ({ img,url }: { img: FinalFrame,url:string }) => (
+const HeaderBlock = ({ img, url }: { img: FinalFrame, url: string }) => (
   <Block
     whileHover={{
       rotate: "2.5deg",
@@ -91,7 +100,7 @@ const HeaderBlock = ({ img,url }: { img: FinalFrame,url:string }) => (
     }}
 
     className="col-span-12 row-span-2 md:col-span-4">
-    <Link href={url}  className="grid relative lg:h-[705px] rounded-l-lg z-10">
+    <Link href={url} className="grid relative lg:h-[705px] rounded-l-lg z-10">
       <Image src={img.imgSrc} fill sizes="(max-width: 768px) 25vw, 50vw" objectFit="cover" priority alt="profile-picture" className=" object-cover rounded-l-2xl" />
       <div className="absolute bottom-5 left-5 text-white border-solid  
         border-b-4 border-white text-3xl">{img.label}</div>
@@ -101,7 +110,7 @@ const HeaderBlock = ({ img,url }: { img: FinalFrame,url:string }) => (
 
 const objBorders = ['', 'rounded-tr-2xl', '', 'rounded-br-2xl']
 
-const SocialsBlock = ({ imgs,urls }: { imgs: FinalFrame[],urls:string[] }) => (
+const SocialsBlock = ({ imgs, urls }: { imgs: FinalFrame[], urls: string[] }) => (
   <>
     {imgs.map((ele, idx) => (
       <Block
@@ -111,7 +120,7 @@ const SocialsBlock = ({ imgs,urls }: { imgs: FinalFrame[],urls:string[] }) => (
         }}
         className="col-span-6 md:col-span-4"
       >
-        <Link href={urls[idx]}  className="grid relative h-[345px]">
+        <Link href={urls[idx]} className="grid relative h-[345px]">
           <Image src={ele.imgSrc} fill sizes="(max-width: 768px) 25vw, 50vw" objectFit="cover" priority alt="profile-picture" className={`object-cover ${objBorders[idx]}`} />
           <div className={`absolute bottom-5 left-5 text-white border-solid  
         border-b-4 border-white text-3xl `}>{ele.label}</div>
