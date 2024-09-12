adfbc0436acb2b53e55eab098319c9c8573ae652
chamba <urgosxd@gmail.com>
2024-09-12 12:43:08 -0700

    aoe

--- src/app/[lng]/(locale-pages)/page.tsx
+++ src/app/[lng]/(locale-pages)/page.tsx
@@ -99,7 +99,7 @@ const removeAccents = (str) => {
     <div className="flex flex-col items-center w-full">
       
       {/* <SwitcherGlobal currentLocale={params.lng}  dynamicLinks={related} slug={undefined}/> */}
-      {/* <CustomCarousel data={t('galleryIni',{returnObjects:true})} ></CustomCarousel> */}
+      <CustomCarousel data={t('galleryIni',{returnObjects:true})} ></CustomCarousel>
       {/* <SessionProvider> */}
       {/* <SessionValidator>GAAAAAAAA</SessionValidator> */}
       {/* </SessionProvider> */}
